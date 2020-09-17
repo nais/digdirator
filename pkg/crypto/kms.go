065f2a77b42a4159edcbe472ad2baeeebec12a33
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2020-09-17 11:58:17 +0200

    wip: merge signer code, rename test package, handle errors

--- pkg/crypto/kms.go
+++ pkg/crypto/kms.go
@@ -1,90 +1,88 @@
 package crypto
 
 import (
-    kms "cloud.google.com/go/kms/apiv1"
-    "context"
-    "crypto/sha256"
-    "fmt"
-    kmspb "google.golang.org/genproto/googleapis/cloud/kms/v1"
-    "gopkg.in/square/go-jose.v2"
-    "gopkg.in/square/go-jose.v2/jwt"
+	kms "cloud.google.com/go/kms/apiv1"
+	"context"
+	"crypto/sha256"
+	"fmt"
+	kmspb "google.golang.org/genproto/googleapis/cloud/kms/v1"
+	"gopkg.in/square/go-jose.v2"
+	"gopkg.in/square/go-jose.v2/jwt"
 )
 
 type KmsKeyPath string
 
 type KmsSigner struct {
-    Client        *kms.KeyManagementClient
-    Ctx           context.Context
-    KmsKeyPath    KmsKeyPath
-    SignerOptions *jose.SignerOptions
+	Client        *kms.KeyManagementClient
+	Ctx           context.Context
+	KmsKeyPath    KmsKeyPath
+	SignerOptions *jose.SignerOptions
 }
 
 func NewKmsSigner(client *kms.KeyManagementClient, ctx context.Context, keyPath KmsKeyPath, signerOpts *jose.SignerOptions) jose.Signer {
-    return &KmsSigner{
-        Client:        client,
-        Ctx:           ctx,
-        KmsKeyPath:    keyPath,
-        SignerOptions: signerOpts,
-    }
+	return &KmsSigner{
+		Client:        client,
+		Ctx:           ctx,
+		KmsKeyPath:    keyPath,
+		SignerOptions: signerOpts,
+	}
 }
 
 func (signer *KmsSigner) Options() jose.SignerOptions {
-    return *signer.SignerOptions
+	return *signer.SignerOptions
 }
 
 func (signer *KmsSigner) Sign(payload []byte) (*jose.JSONWebSignature, error) {
 
-
-
-    return &jose.JSONWebSignature{}, nil
+	return &jose.JSONWebSignature{}, nil
 }
 
 func createSignRequest(keyPath string, content []byte) (*kmspb.AsymmetricSignRequest, error) {
-    digest := sha256.New()
-    if _, err := digest.Write(content); err != nil {
-        return nil, fmt.Errorf("failed to create digest of content: %v", err)
-    }
-    return &kmspb.AsymmetricSignRequest{
-        Name: keyPath,
-        Digest: &kmspb.Digest{
-            Digest: &kmspb.Digest_Sha256{
-                Sha256: digest.Sum(nil),
-            },
-        },
-    }, nil
+	digest := sha256.New()
+	if _, err := digest.Write(content); err != nil {
+		return nil, fmt.Errorf("failed to create digest of content: %v", err)
+	}
+	return &kmspb.AsymmetricSignRequest{
+		Name: keyPath,
+		Digest: &kmspb.Digest{
+			Digest: &kmspb.Digest_Sha256{
+				Sha256: digest.Sum(nil),
+			},
+		},
+	}, nil
 }
 
 // TODO some versions conflict with grpc lib from kms dependency
 func signWithKms(signer jose.Signer, claims jwt.Claims) (string, error) {
-  /*  ctx := context.Background()
-    client, err := kms.NewKeyManagementClient(ctx)
-    if err != nil {
-        return "", fmt.Errorf("could not create key management Client %w", err)
-    }
+	/*  ctx := context.Background()
+	    client, err := kms.NewKeyManagementClient(ctx)
+	    if err != nil {
+	        return "", fmt.Errorf("could not create key management Client %w", err)
+	    }
 
-    content, err := json.Marshal(claims)
-    if err != nil {
-        return "", fmt.Errorf("failed to marshal claims to bytes %w", err)
-    }
+	    content, err := json.Marshal(claims)
+	    if err != nil {
+	        return "", fmt.Errorf("failed to marshal claims to bytes %w", err)
+	    }
 
-    req, err := createSignRequest("keyname", content)
-    if err != nil {
-        return "", fmt.Errorf("could not create sign request %w".err)
-    }
+	    req, err := createSignRequest("keyname", content)
+	    if err != nil {
+	        return "", fmt.Errorf("could not create sign request %w".err)
+	    }
 
-    result, err := client.AsymmetricSign(ctx, req)
-    if err != nil {
-        return "", fmt.Errorf("failed to sign digest: %w", err)
-    }
+	    result, err := client.AsymmetricSign(ctx, req)
+	    if err != nil {
+	        return "", fmt.Errorf("failed to sign digest: %w", err)
+	    }
 
-    signedDigest := result.Signature
+	    signedDigest := result.Signature
 
-    jws := jose.JSONWebSignature{
-        content,
-        []jose.Signature{
+	    jws := jose.JSONWebSignature{
+	        content,
+	        []jose.Signature{
 
-        }
-    }*/
-    return "", nil
-    //return jwt.Signed(signer).Claims(claims).CompactSerialize()
+	        }
+	    }*/
+	return "", nil
+	//return jwt.Signed(signer).Claims(claims).CompactSerialize()
 }
--- pkg/crypto/signer.go
+++ pkg/crypto/signer.go
@@ -1,10 +1,46 @@
 package crypto
 
 import (
+	"bytes"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"gopkg.in/square/go-jose.v2"
 )
+
+const SigningAlg = "RS256"
+
+type ByteSigner interface {
+	SignBytes(payload []byte) ([]byte, error)
+}
+
+type ConfigurableSigner struct {
+	SignerOptions *jose.SignerOptions
+	ByteSigner    ByteSigner
+}
+
+func (ctx *ConfigurableSigner) Options() jose.SignerOptions {
+	return *ctx.SignerOptions
+}
+
+func (ctx *ConfigurableSigner) Sign(payload []byte) (*jose.JSONWebSignature, error) {
+	obj := &jose.JSONWebSignature{}
+	obj.Signatures = make([]jose.Signature, 1)
+
+	headersAndPayload, err := serializeHeadersAndPayload(ctx.SignerOptions, payload)
+	if err != nil {
+		return nil, fmt.Errorf("serializing headers and payload: %w", err)
+	}
+	signatureInfo, err := ctx.ByteSigner.SignBytes(headersAndPayload.Bytes())
+	if err != nil {
+		return nil, fmt.Errorf("signing bytes: %w", err)
+	}
+	var output bytes.Buffer
+	output.WriteString(headersAndPayload.String())
+	output.WriteByte('.')
+	output.WriteString(base64.RawURLEncoding.EncodeToString(signatureInfo))
+	return jose.ParseSigned(output.String())
+}
 
 func SignerFromJwk(jwk *jose.JSONWebKey) (jose.Signer, error) {
 	signerOpts := jose.SignerOptions{}
@@ -16,6 +52,24 @@ func SignerFromJwk(jwk *jose.JSONWebKey) (jose.Signer, error) {
 		return nil, fmt.Errorf("creating jwt signer: %v", err)
 	}
 	return signer, nil
+}
+
+func serializeHeadersAndPayload(opts *jose.SignerOptions, payload []byte) (bytes.Buffer, error) {
+	protected := map[jose.HeaderKey]interface{}{
+		"alg": SigningAlg,
+	}
+	for k, v := range opts.ExtraHeaders {
+		protected[k] = v
+	}
+	serializedProtected, err := json.Marshal(protected)
+	if err != nil {
+		return bytes.Buffer{}, fmt.Errorf("marshalling payload: %w", err)
+	}
+	var headersAndPayloadSerialized bytes.Buffer
+	headersAndPayloadSerialized.WriteString(base64.RawURLEncoding.EncodeToString(serializedProtected))
+	headersAndPayloadSerialized.WriteByte('.')
+	headersAndPayloadSerialized.WriteString(base64.RawURLEncoding.EncodeToString(payload))
+	return headersAndPayloadSerialized, err
 }
 
 func extractX5c(jwk *jose.JSONWebKey) []string {
