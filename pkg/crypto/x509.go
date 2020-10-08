83f311831943df27ca6b0b082ebc4acf1557fce7
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2020-10-08 17:39:13 +0200

    feat: set kid and x5c headers for jwt bearer grant

--- pkg/crypto/x509.go
+++ pkg/crypto/x509.go
@@ -1,28 +1,36 @@
 package crypto
 
 import (
+	"crypto/x509"
 	"encoding/base64"
 	"encoding/pem"
 	"fmt"
-	"io/ioutil"
 )
 
-func LoadPemCertChainToX5C(path string) ([]string, error) {
-	certPEMBlock, err := ioutil.ReadFile(path)
-	if err != nil {
-		return nil, fmt.Errorf("loading PEM cert chain from path %s: %w", path, err)
-	}
+func ConvertPEMBlockToX509Chain(certPEMBlock []byte) ([]*x509.Certificate, error) {
 	var certDERBlock *pem.Block
-	base64EncodedCerts := make([]string, 0)
+	certs := make([]*x509.Certificate, 0)
 	for {
 		certDERBlock, certPEMBlock = pem.Decode(certPEMBlock)
 		if certDERBlock == nil {
 			break
 		}
 		if certDERBlock.Type == "CERTIFICATE" {
-			encoded := base64.StdEncoding.EncodeToString(certDERBlock.Bytes)
-			base64EncodedCerts = append(base64EncodedCerts, encoded)
+			cert, err := x509.ParseCertificate(certDERBlock.Bytes)
+			if err != nil {
+				return nil, fmt.Errorf("while parsing certificate: %w", err)
+			}
+			certs = append(certs, cert)
 		}
 	}
-	return base64EncodedCerts, nil
+	return certs, nil
 }
+
+func ConvertX509CertificatesToX5c(certs []*x509.Certificate) []string {
+	x5c := make([]string, 0)
+	for _, cert := range certs {
+		x5c = append(x5c, base64.StdEncoding.EncodeToString(cert.Raw))
+	}
+	return x5c
+}
+
