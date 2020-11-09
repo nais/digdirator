588653d493f78b5e7bbad76e1a54dca0fb16b2cc
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2020-11-09 17:17:31 +0100

    tests: assert that new correlation IDs are set for each reconciliation

--- controllers/idportenclient/idportenclient_controller_test.go
+++ controllers/idportenclient/idportenclient_controller_test.go
@@ -222,6 +222,7 @@ func TestIDPortenController(t *testing.T) {
 	// update IDPortenClient
 	previousSecretName := cfg.SecretName
 	previousHash := instance.Status.ProvisionHash
+	previousCorrelationID := instance.Status.CorrelationID
 
 	// set new secretname in spec -> trigger update
 	instance.Spec.SecretName = "new-secret-name"
@@ -239,6 +240,7 @@ func TestIDPortenController(t *testing.T) {
 	assert.Len(t, instance.Status.KeyIDs, 2, "should contain two key IDs")
 	assert.Contains(t, instance.Status.KeyIDs, "some-keyid", "previous key should still be valid")
 	assert.Contains(t, instance.Status.KeyIDs, "some-new-keyid", "new key should be valid")
+	assert.NotEqual(t, previousCorrelationID, instance.Status.CorrelationID, "should generate new correlation ID")
 
 	// new secret should exist
 	assertSecretExists(t, instance.Spec.SecretName, cfg.NamespaceName, instance)
