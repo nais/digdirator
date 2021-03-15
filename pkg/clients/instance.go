015160bfe348203cabc517fdb4768e53238e85b0
ybelMekk <youssef.bel.mekki@nav.no>
2021-03-15 09:01:09 +0100

    add: switch for setting annotations for right resource (idporten or maskinporten) to be set.

--- pkg/clients/instance.go
+++ pkg/clients/instance.go
@@ -25,7 +25,6 @@ type Instance interface {
 	metav1.Object
 	runtime.Object
 	schema.ObjectKind
-	metav1.ObjectMetaAccessor
 	Hash() (string, error)
 	GetStatus() *nais_io_v1.DigdiratorStatus
 	SetStatus(status nais_io_v1.DigdiratorStatus)
