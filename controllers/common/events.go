0ee16d67bd02b965238bbfa9ac419fd69130e9bd
Trong Huu Nguyen <trong.huu.nguyen@nav.no>
2024-09-05 12:27:54 +0200

    feat(controllers); remove unused events

--- controllers/common/events.go
+++ controllers/common/events.go
@@ -4,12 +4,9 @@ const (
 	// Machine readable event "Reason" fields, used for determining synchronization state.
 	EventSynchronized               = "Synchronized"
 	EventFailedSynchronization      = "FailedSynchronization"
-	EventFailedStatusUpdate         = "FailedStatusUpdate"
 	EventCreatedInDigDir            = "CreatedInDigDir"
 	EventUpdatedInDigDir            = "UpdatedInDigDir"
 	EventRotatedInDigDir            = "RotatedInDigDir"
-	EventSkipped                    = "Skipped"
-	EventRetrying                   = "Retrying"
 	EventActivatedScopeInDigDir     = "ActivatedScopeInDigDir"
 	EventDeactivatedScopeInDigDir   = "DeactivatedScopeInDigDir"
 	EventCreatedScopeInDigDir       = "CreatedScopeInDigDir"
